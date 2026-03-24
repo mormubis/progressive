60e45300c93733cbfdd64f722c3bdc47e052ebb8
Adrian de la Rosa <github@brick.email>
2026-03-24 13:07:11 +0100

    release: @echecs/progressive@0.2.2
    Format fix for Prettier compliance.

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,7 +1,9 @@
 import type { Game } from './types.js';
 
 function gamesForPlayer(playerId: string, games: Game[][]): Game[] {
-  return games.flat().filter((g) => g.whiteId === playerId || g.blackId === playerId);
+  return games
+    .flat()
+    .filter((g) => g.whiteId === playerId || g.blackId === playerId);
 }
 
 export { gamesForPlayer };
